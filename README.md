1b3ecbdc477ff3a551e2a1aaefd7a99b7cff47b8
Jon Borgonia <jon@gomagames.com>
2015-05-24 18:03:54 -1000

    adds specs for loan()

--- README.md
+++ README.md
@@ -20,6 +20,32 @@ Use strict mode for this exercise. ![Test][test]
 
 ### loan()
 
+Declare a function named `loan()` that takes `0` arguments ![Test][test]  
+
+Write the following statements inside the `loan()` function block
+
+1. Declare a variable named `account`, with an initial value being a literal object having the following properties and values:
+  - key : `borrowed`, value : `550000`
+  - key : `balance`, value : `286000`,
+  - key : `monthlyPayment`, value : `1700`,
+  - key : `defaulted`, value : `0`,
+  - key : `defaultsToForeclose`, value : `5`,
+  - key : `foreclosed`, value : `false`
+1. Declare a function named `missPayment` that takes `0` arguments.
+  - Access the `defaulted` property of the `account` variable and increase it's value by `1`.
+  - Write a conditional that, when the value of `account.defaulted` is greater than or equal to `account.defaultsToForeclose`, will run the following statement:
+    - set the value of the `foreclosed` property of the `account` object to `true`
+1. return a literal object with the following properties:
+  - key : `getBalance`, value : an unnamed function expression that takes `0` arguments. ![Test][test]
+    - Create a closure by returning the value of `balance` by accessing that property of the locally scoped variable `account`. ![Test][test]
+  - key : `receivePayment`, value : an unnamed function expression that takes `1` argument named `amount`. ![Test][test]
+    - Create a conditional statement that will run the following statements when `amount` is less than `account.monthlyPayment` :
+      - invoke the `missPayment` function with `0` arguments
+    - Finally, decrement the value of the `balance` property of the `account` variable, by the value of the `amount` parameter. ![Test][test]
+  - key : `getMonthlyPayment`, value : an unnamed function expression that takes `0` arguments. ![Test][test]
+    - Create a closure by returning the value of `monthlyPayment` by accessing that property of the locally scoped variable `account`. ![Test][test]
+  - key : `isForeclosed`, value : an unnamed function expression that takes `0` arguments. ![Test][test]
+    - Create a closure by returning the value of `foreclosed` by accessing that property of the locally scoped variable `account`. ![Test][test]
 
 
 
--- test/foreclosure-spec.js
+++ test/foreclosure-spec.js
@@ -30,4 +30,96 @@ describe('foreclosure', function() {
     foreclosure.search('var monthsUntilEvicted;').should.be.above(-1);
   });
 
+  it('should declare a function named `loan()`', function() {
+    expect(GLOBAL.loan).to.exist;
+    (typeof GLOBAL.loan).should.equal('function');
+  });
+
+  describe('loan()', function() {
+    
+    describe('returns a literal object that', function() {
+      
+      var loan;
+
+      beforeEach(function () {
+        loan = GLOBAL.loan();
+      });
+
+      describe('has a key named `getBalance`', function() {
+        
+        it('should be an unnamed function expression', function() {
+          loan.should.have.property('getBalance');
+          (typeof loan.getBalance).should.equal('function');
+        });
+
+        it('should create a closure when the function is invoked, and returns the value of the `balance` property of `account`', function() {
+          loan.getBalance().should.equal(286000);
+        });
+
+      });
+
+      describe('has a key named `receivePayment`', function() {
+        
+        it('should be an unnamed function expression', function() {
+          loan.should.have.property('receivePayment');
+          (typeof loan.receivePayment).should.equal('function');
+        });
+
+        it('should conditionally invoke the `missPayment` function', function() {
+          // hard to test
+        });
+
+        it('should decrement `account.balance` by `amount`', function() {
+          loan.receivePayment(2000);
+          loan.getBalance().should.equal(284000);
+          loan.receivePayment(20000);
+          loan.getBalance().should.equal(264000);
+        });
+
+      });
+      
+      describe('has a key named `getMonthlyPayment`', function() {
+        
+        it('should be an unnamed function expression', function() {
+          loan.should.have.property('getMonthlyPayment');
+          (typeof loan.getMonthlyPayment).should.equal('function');
+        });
+
+        it('should create a closure when the function is invoked, and returns the value of the `monthlyPayment` property of `account`', function() {
+          loan.getMonthlyPayment().should.equal(1700);
+        });
+
+      });
+
+      describe('has a key named `isForeclosed`', function() {
+        
+        it('should be an unnamed function expression', function() {
+          loan.should.have.property('isForeclosed');
+          (typeof loan.isForeclosed).should.equal('function');
+        });
+
+        it('should create a closure when the function is invoked, and returns the value of the `monthlyPayment` property of `account`', function() {
+          loan.isForeclosed().should.equal(false);
+        });
+
+        it('should be true when payments default 5 times', function() {
+          loan.isForeclosed().should.equal(false);
+          loan.receivePayment(0);
+          loan.isForeclosed().should.equal(false);
+          loan.receivePayment(0);
+          loan.isForeclosed().should.equal(false);
+          loan.receivePayment(0);
+          loan.isForeclosed().should.equal(false);
+          loan.receivePayment(0);
+          loan.isForeclosed().should.equal(false);
+          loan.receivePayment(0);
+          loan.isForeclosed().should.equal(true);
+        });
+
+      });
+
+    });
+
+  });
+
 });
